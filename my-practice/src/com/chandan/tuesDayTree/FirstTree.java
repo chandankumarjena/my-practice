4b31295482c1bb72c53556a476e4351324b57410
chandankumarj <chandankumarj@chandankumarj-macbookpro>
2020-10-28 11:39:38 +0530

    tree

---
+++ my-practice/src/com/chandan/tuesDayTree/FirstTree.java
@@ -0,0 +1,21 @@
+package com.chandan.tuesDayTree;
+
+public class FirstTree {
+	
+	public Node createNewNode(int data) {
+		return new Node(data);
+	}
+	public static void main(String []args) {
+		FirstTree a = new FirstTree();
+	    
+	    Node root = a.createNewNode(2);
+	    root.left = a.createNewNode(7);
+	    root.right = a.createNewNode(5);
+	    root.left.left = a.createNewNode(2);
+	    root.left.right = a.createNewNode(6);
+	    root.left.right.left = a.createNewNode(5);
+	    root.left.right.right = a.createNewNode(11);
+	    root.right.right = a.createNewNode(9);
+	    root.right.right.left = a.createNewNode(4);
+	}
+}
